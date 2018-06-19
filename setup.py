71fdf891e305ddcf9f0e1d0d2d4eb3811bf4785e
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-06-19 13:47:43 -0400

    Updated some items and added others items for the package metadata

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.1'
+version = '1.4'
 
 setup(
         name='plonetheme.transition',
@@ -9,12 +9,13 @@ setup(
         long_description=open('README.rst', 'rb').read()+'\n'+
                          open(os.path.join("docs", "INSTALL.txt")).read()+'\n'+
                          open(os.path.join("docs", "HISTORY.txt")).read(),
-        version='1.1',
+        version=version,
         author='Guido Stevens',
         author_email='guido.stevens@cosent.net',
         maintainer='Leonardo Caballero',
         maintainer_email='leonardocaballero@gmail.com',
         url='http://github.com/gyst/plonetheme.transition',
+        license='GPL',
         packages=find_packages(),
         include_package_data=True,
         namespace_packages=[
@@ -38,6 +39,7 @@ setup(
             "Framework :: Zope3",
             "Intended Audience :: Developers",
             "Intended Audience :: End Users/Desktop",
+            "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
             "Operating System :: OS Independent",
             "Programming Language :: Python",
             "Programming Language :: Python :: 2.6",
@@ -50,5 +52,3 @@ setup(
             'z3c.autoinclude.plugin': 'target = plone',
             }
         )
-
-
